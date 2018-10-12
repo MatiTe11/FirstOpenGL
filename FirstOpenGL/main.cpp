8c475bd53ec928f32cb8ac946574c0bee7683acb
Mateusz Tęgowski <m.tegowski@wp.pl>
2018-10-12 14:34:54 +0200

    unnecessary code deleted

--- FirstOpenGL/main.cpp
+++ FirstOpenGL/main.cpp
@@ -27,30 +27,7 @@ int main()
 	float screenWidth = 800;
 	float screenHeight = 600;
 
-	float vertices[] = {
-		0.9f,  0.5f, 0.0f,  1.0f, 1.0f,  // top right
-		0.9f, -0.5f, 0.0f,  1.0f, 0.0f,  // bottom right
-		0.1f, -0.5f, 0.0f,  0.0f, 0.0f,  // bottom left
-		0.1f,  0.5f, 0.0f,  0.0f, 1.0f   // top left 
-	};
-
-	float vertices2[] = {
-		-0.9f,  0.5f, 0.0f,  // top right
-		-0.9f, -0.5f, 0.0f,  // bottom right
-		-0.1f, -0.5f, 0.0f,  // bottom left
-		-0.1f,  0.5f, 0.0f   // top left 
-	};
-
-	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
-	};
- 
-	glm::mat4 trans;
-	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
-	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
 	
-
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -71,48 +48,14 @@ int main()
 		return -1;
 	}
 
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, screenWidth, screenHeight);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	int nrAttributes;
-	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-	std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
 
 	ShaderProgram program1("vertexShader.txt", "fragmentShader1.txt");
 	ShaderProgram program2("vertexShader.txt", "fragmentShader2.txt");
 
-	///first VAO
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	//creating second vao
-	unsigned int VAO2;
-	glGenVertexArrays(1, &VAO2);
-	glBindVertexArray(VAO2);
-
-	//creating second vbo
-	unsigned int VBO2;
-	glGenBuffers(1, &VBO2);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	//binding exisisting ebo
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	
 
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
@@ -150,12 +93,6 @@ int main()
 	program2.setSampler2d("ourTexture2", 1);
 
 
-	///attrib for first set of verticies
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
 
 	//matricies
 	glm::mat4 view;
@@ -172,18 +109,9 @@ int main()
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
-		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
 		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		/*program2.set1f("alpha", sin(glfwGetTime()));
-		program2.setMatrix4f("transform", glm::value_ptr(trans));
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		program1.set4f("ourColor", 1.0f, sin(glfwGetTime()) / 2 + 0.5f, 1.0f, 1.0f);
-		program1.setMatrix4f("transform", glm::value_ptr(trans));
-
-		glBindVertexArray(VAO2);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);*/
+		
 		program2.setMatrix4f("model", glm::value_ptr(*myCube.getModel()));
 		program2.setMatrix4f("view", glm::value_ptr(view));
 		program2.setMatrix4f("projection", glm::value_ptr(projection));
